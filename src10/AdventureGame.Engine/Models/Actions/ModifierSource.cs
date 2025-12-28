0ad7a6db9b279d807272df0fc149dff57012e534
Gabriel Hyter <ghyter@hotmail.com>
2025-12-28 14:37:35 -0500

    Remove DifficultyCheck class from ModifierSource.cs
    Deleted the DifficultyCheck class and its properties from ModifierSource.cs, leaving only the ModifierSource class definition in the file. This streamlines the file to focus solely on ModifierSource.

--- src10/AdventureGame.Engine/Models/Actions/ModifierSource.cs
+++ src10/AdventureGame.Engine/Models/Actions/ModifierSource.cs
@@ -1,12 +1,4 @@
-#nullable enable
-
 namespace AdventureGame.Engine.Models.Actions;
-
-public sealed class DifficultyCheck
-{
-    public int Difficulty { get; set; } = 0;
-    public List<ModifierSource> Modifiers { get; set; } = [];
-}
 
 public sealed class ModifierSource
 {
